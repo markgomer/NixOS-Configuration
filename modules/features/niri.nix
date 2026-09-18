c44b188c4281753b2ccd80cff9ac16bbf36806e6
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-09-18 12:47:01 -0300

    update some tools for the desktop

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -28,11 +28,20 @@
             pkgs.brightnessctl # laptop brightness
             pkgs.nwg-look # GTK theme configurator
             pkgs.pavucontrol # audio GUI fallback
-            pkgs.wayland-pipewire-idle-inhibit # idle inhibition
+            pkgs.pulseaudio # for audio switching
+            pkgs-unstable.wayland-pipewire-idle-inhibit # idle inhibition
             pkgs.playerctl # for using keyboard media keys
-            pkgs.ianny # break reminder program
-            pkgs.xwayland-satellite
+            pkgs-unstable.ianny # break reminder program
+            pkgs-unstable.xwayland-satellite
             pkgs.swaybg # wallpaper
+            # screen toolkit noctalia plugin
+            pkgs-unstable.slurp # region selection
+            pkgs-unstable.grim # screen capt
+            pkgs-unstable.hyprpicker # color pick
+            pkgs-unstable.imagemagick # image processing
+            pkgs-unstable.zbar # qr barcode scanning
+            pkgs-unstable.bc # GIF duration and framrate calculations
+            pkgs-unstable.mpv
         ];
     };
 }
